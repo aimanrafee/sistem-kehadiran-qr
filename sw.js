d0570430981c21af73adde52d58259e763e4682f
Aiman Mohd <39325438+aimanrafee@users.noreply.github.com>
2026-02-04 16:19:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,43 @@
-const CACHE_NAME = 'qr-offline-v1';
+const CACHE_NAME = 'qr-keluarga-v2';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/script.js',
-    '/manifest.json',
+    './',
+    './index.html',
+    './script.js',
+    './manifest.json',
     'https://unpkg.com/html5-qrcode'
 ];
 
+// Pemasangan Service Worker
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Caching assets...');
+            return cache.addAll(ASSETS);
+        })
+    );
 });
 
+// Pembersihan Cache Lama
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Strategi Fetch (Offline First)
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request).catch(() => {
+                if (e.request.mode === 'navigate') {
+                    return caches.match('./index.html');
+                }
+            });
+        })
+    );
 });
